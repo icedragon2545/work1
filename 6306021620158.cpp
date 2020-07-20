cbef34519e2f25b922a12446db5cbd65deb7fb4d
icedragon2545 <68541205+icedragon2545@users.noreply.github.com>
2020-07-20 18:09:10 +0700

    Add files via upload

---
+++ 6306021620158.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "1st Term of 2020   6306021620158  Burapha Suriwan"<<endl;
+	cout << "___________________________________________________________________________"<<endl;
+	cout << "| Sudject ID  |  Sudject name                | Score (100) | Grad |"<<endl;
+	cout << "|_____________|______________________________|_____________|______|__________"<<endl;
+	cout << "|  080103001  | ENGLISH 1                    |             |      |"<<endl;
+	cout << "|  040203100  | GENERAL MATHEMATICS          |             |      | "<<endl;
+	cout << "|  060243103  | PROBLEM SOLVING IN INFORMATI |             |      |"<<endl;
+	cout << "|  060243102  | COMPUTER PROGRAMMING         |             |      | "<<endl;
+	cout << "|  080303606  | SYSTEMATIC AND CREATIVE THIN |             |      |" <<endl;
+	cout << "|  060243101  | FUNDAMENTAL OF INFORMATION T |             |      | "<<endl;
+	cout << "|  080303518  | AEROBIC DANCE & COVER DANCE  |             |      |" <<endl;
+	cout << "____________________________________________________________________________"<<endl;
+	cout << "|  TOTAL      | Subject = 7                  | Average =          |" <<endl;
+	cout << "============================================="<<endl;
+	return (0);
+}
